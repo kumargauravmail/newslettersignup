bbc8fa16cbaa97834629bd0c1b93055c9c3a16c5
Gaurav BranchDrupal8From <kumargauravmail@yahoo.com>
2019-04-26 11:36:55 +0530

    Adding newsletter signup module

---
+++ README.txt
@@ -0,0 +1,27 @@
+Newsletter Signup
+=================
+
+Newsletter Signup is a simple module which displays a form for the site visitors to signup for a newsletter.
+The form simply has three fields- name, company name and email address. Company name is a optional field.
+The form is available as a independent page and also as a block under admin block configuration.
+
+Instructions
+------------
+
+Download and install this module as a regular drupal module installation. This module is not dependent on any other module.
+After installation, it is required to configure the email address to which the information of the signup form is to be sent.
+
+A new menu will be added in the Drupal admin configuration page. 
+This can be configured at "/admin/config/newslettersignup/adminsettings".
+
+The newsletter signup form will be available at "/newsletter-signup". 
+Optionally you can provide a menu link to this form incase you do not want to use it as a block.
+
+Newsletter Signup block will be available under Blocks page to add the form as a Block to any region.
+
+Upcoming Features
+-----------------
+
+1. The newsletter signup information to be saved in database.
+2. A new page to be available in Drupal admin to manage newsletter signup's.
+3. Newsletter signup email notification to be made optional.
---
+++ src/Form/NewsletterSignupForm.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\newslettersignup\Form\NewsletterSignupForm.
+ */
+
+namespace Drupal\newslettersignup\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Newsletter signup form.
+ */
+class NewsletterSignupForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'newslettersignup_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['newsletteryourname'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Name'),
+      '#size' => 60,
+      '#maxlength' => 60,
+      '#required' => TRUE,
+    );	 
+    $form['newslettercompanyname'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Company Name'),
+      '#size' => 60,
+      '#maxlength' => 100,
+    );	 
+    $form['newsletteryouremail'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Email'),
+      '#size' => 60,
+      '#maxlength' => 100,
+      '#required' => TRUE,
+    );	 
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Signup'),
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    /**
+     * Validate email address
+     */
+    $newsletteryouremail = trim($form_state->getValue('newsletteryouremail'));
+  
+    if ($newsletteryouremail !== '' && !\Drupal::service('email.validator')->isValid($newsletteryouremail)) {
+      $form_state->setErrorByName('newsletteryouremail', $this->t('Invalid email address'));  
+    }
+	
+	/**
+	 * Validate name
+	 * @todo code better validation for name
+     */
+    $newsletteryourname = trim($form_state->getValue('newsletteryourname'));
+  
+    if (is_numeric($newsletteryourname)) {
+	  $form_state->setErrorByName('newsletteryourname', $this->t('Invalid characters in name'));  
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    /**
+     * Get user form input
+     */
+    $newsletteryourname = trim($form_state->getValue('newsletteryourname'));
+    $newslettercompanyname = trim($form_state->getValue('newslettercompanyname'));
+    $newsletteryouremail = trim($form_state->getValue('newsletteryouremail'));
+    
+    /**
+     * Get the email address to which email to be sent 
+     */
+    $config = $this->config('newslettersignup.adminsettings');  
+    $newslettersignup_admin_email = trim($config->get('newslettersignup_admin_email'));
+    
+    if ($newslettersignup_admin_email) {
+      /**
+       * Send email
+       */
+      $mail_manager = \Drupal::service('plugin.manager.mail');
+      $langcode = \Drupal::currentUser()->getPreferredLangcode();
+      $params['message']['newsletteryourname'] = $newsletteryourname;
+      $params['message']['newsletteryouremail'] = $newsletteryouremail;
+      $params['message']['newslettercompanyname'] = $newslettercompanyname;
+      $to = $newslettersignup_admin_email;
+      
+      $result = $mail_manager->mail('newslettersignup', 'signup_notify', $to, $langcode, $params, NULL, 'true');
+	  drupal_set_message(t('Thanks ' . $newsletteryourname . '! You have been successfully signed up.'));
+   }
+  }
+ 
+}
